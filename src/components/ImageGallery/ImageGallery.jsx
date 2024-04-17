e36a3b82a2b404e0ad33398f9bd784c3e7bc9532
Exxin <exxinblood@gmail.com>
2024-04-17 16:05:03 +0300

    л

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,18 +1,4 @@
 
-// import ImageCard from "../ImageCard/ImageCard";
-
-// export default function ImageGallery({ items }) {
-//   return (
-//     <ul>
-//       {items && items.map((image) => (
-//         <li key={image.id}>
-//           <ImageCard image={image} />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
 import ImageCard from "../ImageCard/ImageCard";
 
 export default function ImageGallery({ items, onImageClick }) {
